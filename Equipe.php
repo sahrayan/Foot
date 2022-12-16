f807c9435ee203b7527107d6c8e1e8ba4e638872
Rayan R <rayanraihani9@gmail.com>
2022-12-16 14:07:16 +0100

    classe quipe jai rajouter les nom et les pays des joueur qui sont dans une equipe

--- Equipe.php
+++ Equipe.php
@@ -1,6 +1,18 @@
 <?php
 class Equipe{
     private $_joueur;
+    private $_nom;
+    private $_pays;
+    public function __construct($joueur,$nom,$pays){
+        $this->_nom=$nom;
+        $this->_joueur=[];
+        $this->_pays = $pays;
+
+    }
+    public function getNom(){
+        return $this->_nom;
+    }
+    
 
     
 }
