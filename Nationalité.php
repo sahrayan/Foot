63e16e3b061a61024a5967696d5db09a488d0f49
rayan <rayanraihani9@gmail.com>
2022-12-20 14:37:10 +0100

    ajouter au joueur la nationalité

--- Nationalité.php
+++ Nationalité.php
@@ -7,19 +7,23 @@ class Nationalite{
         $this->_nom = $nom;
         $this->_joueur = [];
     }
-    public function getNom(){
-        return $this->_nom;
-    }
-    public function setNom(){
-        $this->_nom = $nom;
-    }
-    public function setNationalite(){
-        $this->_nationalite= $nationalite;
+    
+    public function getJoueur(){
+        return $this->_joueur;
     }
 
     public function addJoueur($nouveauJoueur){
         $this->_joueur[] = $nouveauJoueur;
     }
+
+    public function getNom():string {
+        return $this->_nom;
+    }
+
+    // public function setNationalite(){
+    //     $this->_nationalite = $nationalite;
+    // }
+
 
         
 }
