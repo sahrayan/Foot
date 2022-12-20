79cf582a2acf000d570be78aee714b7ef2fefd5e
rayan <rayanraihani9@gmail.com>
2022-12-20 16:51:52 +0100

    juste besoin du nom du joueur

--- Nationalité.php
+++ Nationalité.php
@@ -1,20 +1,12 @@
 <?php
 class Nationalite{
     private string $_nom;
-    private $_joueur;
 
     public function __construct( string $nom){
         $this->_nom = $nom;
-        $this->_joueur = [];
-    }
-    
-    public function getJoueur(){
-        return $this->_joueur;
     }
 
-    public function addJoueur($nouveauJoueur){
-        $this->_joueur[] = $nouveauJoueur;
-    }
+
 
     public function getNom():string {
         return $this->_nom;
