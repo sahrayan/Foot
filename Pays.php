469d99c21c8d218e33e3ef758785d402ba7c42d2
Rayan R <rayanraihani9@gmail.com>
2022-12-21 09:34:00 +0100

    changement de nom par pays c est plus parlant et ajout de addequipe

--- Pays.php
+++ Pays.php
@@ -1,16 +1,17 @@
 <?php
 class Pays{
-    private string $_nom;
+    private string $_pays;
     private array $_equipe;
-    public function __construct($nom){
-        $this->_nom = $nom;
+    public function __construct($pays){
+        $this->_pays = $pays;
         $this->_equipe = [];
     }
-    public function getNom(){
-        return $this->_nom;
+    public function getPays(){
+        return $this->_pays;
     }
-    public function getEquipe(){
-    return $this->_equipe;
+// ajouter une equie dans equipe
+    public function addEquipe($equipe){
+        $this->_equipe[] = $equipe;
     }
 // rajout de l'equipe dans Pays
     public function addPays($nouvPays){
