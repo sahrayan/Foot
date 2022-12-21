487c2f760d645cccec845705f3027c3345d4d3ac
Rayan R <rayanraihani9@gmail.com>
2022-12-21 09:53:33 +0100

    ajout du joueur et de la fonction

--- Nationalité.php
+++ Nationalité.php
@@ -1,20 +1,30 @@
 <?php
 class Nationalite{
-    private string $_nom;
+    private string $_nationalite;
 
-    public function __construct( string $nom){
-        $this->_nom = $nom;
+    public function __construct( string $nationalite){
+        $this->_nationalite = $nationalite;
+        $this->_joueur = [];
+    }
+    public function getNationalite():string {
+        return $this->_nationalite;
+    }
+    public function addJoueur($joueur){
+        $this->_joueur[] = $joueur;
+    }
+    // display Nationaliter des joueurs
+    public function dispNationalite(){
+        $result ="les joueurs on la Nationalité ".$this. "<br>";
+        foreach(->_joueur as $joueur){
+            $result .= $joueur ."<br>";
+        }
+        return $result;
+    }
+    public function __toString(){
+        return $this->getNationalite();
     }
 
-
-
-    public function getNom():string {
-        return $this->_nom;
-    }
-
-    // public function setNationalite(){
-    //     $this->_nationalite = $nationalite;
-    // }
+   
 
 
         
