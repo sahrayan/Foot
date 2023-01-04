728d6ffcd28326066fc57f90abe27cdb5fddbbee
Rayan <rayanraihani9@gmail.com>
2023-01-04 14:35:46 +0100

    creation du joueur 1

--- index.php
+++ index.php
@@ -6,15 +6,15 @@ spl_autoload_register(function ($class_name) {
 });
 // Nationalité
 $n1 = new Nationalite('Fr');
-$n2 = new Nationalite('Esp');
+// $n2 = new Nationalite('Esp');
 // Pays
-$p1 = new Pays('France');
+// $p1 = new Pays('France');
 $p2 = new Pays('Espagne');
-// Equipe
-$t1 = new Equipe('PSG',$p1);
-$t2 = new Equipe('Barça',$p2);
+// // Equipe
+// $t1 = new Equipe('PSG',$p1);
+// $t2 = new Equipe('Barça',$p2);
 $t3 = new Equipe('Real',$p2);
-$t4 = new Equipe('OM',$p1);
+// $t4 = new Equipe('OM',$p1);
 // Joueur
 $j1 = new Joueur('Amrabat','Sofyan',26,$n1,$t3);
 // $j2 = new Joueur('Bounou','Yassine',31,$n1,$t3);
